3469acca9d94e97ad1d0e6721d379c4a57e6cab2
Mateusz Safarzyński <146592337+safariots@users.noreply.github.com>
2023-11-18 22:18:00 +0100

    v1.1

---
+++ funkcja_random.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int losowa = random.Next(1, 101);
+
+        Console.WriteLine("Zgadnij liczbę (1 - 100): ");
+
+        while (true)
+        {
+            string liczba = Console.ReadLine();
+
+            if (int.TryParse(liczba, out int zgadywana))
+            {
+                if (zgadywana < 1 || zgadywana > 100)
+                {
+                    Console.WriteLine("Podaj liczbę z zakresu 1-100.");
+                }
+                else if (zgadywana == losowa)
+                {
+                    Console.WriteLine("Gratulacje, zgadłeś!");
+                    break;
+                }
+                else if (zgadywana < losowa)
+                {
+                    Console.WriteLine("Za mało, spróbuj ponownie.");
+                }
+                else
+                {
+                    Console.WriteLine("Za dużo, spróbuj ponownie.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Podaj poprawną liczbę.");
+            }
+        }
+    }
+}
---
+++ instrukcje_warunkowe.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program()
+{
+    static void Main()
+    {
+        // instrukcja if
+
+        int liczba = 10;
+
+        if (liczba > 0)
+        {
+            Console.WriteLine("Liczba jest dodatnia");
+        }
+
+        // instrukcja else if (dodatkowy warunek kiedy if jest nie spełnione)
+
+        int liczba1 = 0;
+
+        if (liczba1 > 0)
+        {
+            Console.WriteLine("Liczba jest dodatnia");
+        }
+        else if (liczba1 > 0)
+        {
+            Console.WriteLine("Liczba jest ujemna");
+        }
+        else
+        {
+            Console.WriteLine("Liczba jest równa zeru");
+        }
+
+        // insturkcja switch umożliwia porównywanie wielu wartości
+
+        int dayOfWeek = 3;
+
+        switch (dayOfWeek)
+        {
+            case 1:
+                Console.WriteLine("Poniedziałek");
+                break;
+            case 2:
+                Console.WriteLine("Wtorek");
+                break;
+            case 3:
+                Console.WriteLine("Środa");
+                break;
+
+            // inne przypadki
+            default:
+                Console.WriteLine("Nieznany dzień tygodnia");
+                break;
+        }
+
+        // && - AND, || - OR, ! - NOT
+        int x = 5;
+        int y = 10;
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Obie liczby są dodatnie.");
+        }
+        if (x > 0 || y > 0)
+        {
+            Console.WriteLine("Co najmniej jedna z liczb jest dodatnia.");
+        }
+        if (!(x < 0))
+        {
+            Console.WriteLine("x nie jest ujemne.");
+        }
+    }  
+}
+
+
